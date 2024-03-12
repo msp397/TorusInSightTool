7c025bce081b8ccc8f0bd4e04e65a5450941d464
msp397 <marim@torus.tech>
2024-03-12 19:31:39 +0530

    add stream table

--- src/Datatable/ShowSpace.tsx
+++ src/Datatable/ShowSpace.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "react-json-view-lite/dist/index.css";
 import { JSONTree } from "react-json-tree";
-import {ScrollShadow} from "@nextui-org/react";
+import { ScrollShadow } from "@nextui-org/react";
 
-const ShowSpace = ({ data, selectedKey, selectedDataType }:any) => {
-
-
-
-  const isEmptyObjectOrArray = (value : any) => {
+const ShowSpace = ({ data, selectedKey, selectedDataType }: any) => {
+  const isEmptyObjectOrArray = (value: any) => {
     if (Array.isArray(value)) {
       return value.length === 0;
     }
-  
-    if (typeof value === 'object' && value !== null) {
+
+    if (typeof value === "object" && value !== null) {
       return Object.keys(value).length === 0;
     }
-  
+
     return true; // Return true for non-object and non-array values
   };
 
@@ -56,12 +53,39 @@ const ShowSpace = ({ data, selectedKey, selectedDataType }:any) => {
     // Regular expressions
   };
   const Key = selectedKey.split(":").join(": ");
+
+  function Split(data: any) {
+    const odd = data.filter((v: any, id: any) => id % 2);
+    const even = data.filter((v: any, id: any) => !(id % 2));
+
+    return (
+      <tr className="border-b dark:bg-gray-800 dark:border-gray-700">
+        <td className="px-6">
+          {even.map((ele: any, index: any) => (
+            <div key={index}>
+              <h2>{ele}</h2>
+              <br />
+            </div>
+          ))}
+        </td>
+        <td className="px-6">
+          {odd.map((ele: any, index: any) => (
+            <div key={index}>
+              <h2>{ele}</h2>
+              <br />
+            </div>
+          ))}
+        </td>
+      </tr>
+    );
+  }
+
   return (
     <div className="flex flex-col w-full h-full justify-start items-center">
       <h2 className="text-center font-bold px-3 h-[5%] bg-gray-400 mb-5">
         {Key}
       </h2>
-      {["list", "zset", "hash", "set", "stream"].includes(selectedDataType) ? (
+      {["list", "zset", "hash", "set"].includes(selectedDataType) ? (
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -85,24 +109,58 @@ const ShowSpace = ({ data, selectedKey, selectedDataType }:any) => {
             ))}
           </tbody>
         </table>
-      ) : selectedDataType == "string" ? (
+      ) : ["stream"].includes(selectedDataType) ? (
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Entity ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Data
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item: any, index: any) => (
+              <tr
+                key={index}
+                className=" border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="px-6 py-4">{item[0]}</td>
+                <td className="px-6 py-4">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th scope="col" className="px-6 py-3">
+                          Key
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Value
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>{Split(item[1])}</tbody>
+                  </table>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : selectedDataType === "string" ? (
         <>
           <h2 className="text-center font-bold text-gray-400">{data}</h2>
         </>
       ) : (
-        <div className="w-full h-full box-border-2 shadow-lg p-5 mb-5 overflow-auto ">
-        <ScrollShadow hideScrollBar className="w-full h-full" >
-          {isEmptyObjectOrArray(data) ? (
-            <p className="p-3">No data available</p>
-          ) : (
-            <JSONTree
-              data={data}
-              theme={lightTheme}
-              hideRoot
-            />
-          )}
-        </ScrollShadow>
-      </div>
+        <div className="w-full h-full box-border-2 shadow-lg p-5 mb-5 overflow-auto">
+          <ScrollShadow hideScrollBar className="w-full h-full">
+            {isEmptyObjectOrArray(data) ? (
+              <p className="p-3">No data available</p>
+            ) : (
+              <JSONTree data={data} theme={lightTheme} hideRoot />
+            )}
+          </ScrollShadow>
+        </div>
       )}
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,9 @@ interface Key {
   type: string;
 }
 
-
 export default function Home() {
   const [redisView, setRedisView] = React.useState<Key>({ key: "", type: "" });
-  const [data, setData] = React.useState(null); 
-
+  const [data, setData] = React.useState(null);
 
   const { data: session, status } = useSession({
     required: true,
@@ -27,20 +25,24 @@ export default function Home() {
 
   return (
     <main className="flex w-full h-[93vh] mt-3">
-      <div className={`${data ? "overflow-y-auto overflow-x-hidden px-2 w-3/4" : 'w-full px-2'}`}>
+      <div
+        className={`${
+          data ? "overflow-y-auto overflow-x-hidden px-2 w-3/4" : "w-full px-2"
+        }`}
+      >
         <ScrollShadow hideScrollBar className="w-full h-full">
-      <App setRedisView={setRedisView} setData={setData}/>
-      </ScrollShadow>
+          <App setRedisView={setRedisView} setData={setData} />
+        </ScrollShadow>
       </div>
       {data && (
-      <div className={` ${data ? "overflow-auto w-1/4 pl-2 mt-5" : null}`}>
+        <div className={` ${data ? "overflow-auto w-1/4 pl-2 mt-5" : null}`}>
           <ShowSpace
             data={data}
             selectedKey={redisView.key}
             selectedDataType={redisView.type}
           />
-      </div>
-        )}
+        </div>
+      )}
     </main>
   );
 }
